2b1f9bd8d312acaabf96ce33f00be0b2cadf028f
voidSevenSevenSix <cobbsaladluv@gmail.com>
2026-07-07 17:13:12 -0500

    passenger class

--- route/entity/Passenger.java
+++ route/entity/Passenger.java
@@ -1,5 +1,37 @@
 package route.entity;
 
 public class Passenger {
-    
+    private final int destinationStopId;
+
+    private int ticksSpentWaiting = 0;
+    private int ticksSpentTraveling = 0;
+    private boolean isOnBus = false;
+
+
+    public Passenger(int destinationStopId){
+        this.destinationStopId = destinationStopId;
+    }
+
+    public void tick(){
+        if(!isOnBus){
+            ticksSpentWaiting++;
+        }
+        ticksSpentTraveling++;
+    }
+
+    public void setOnBus(boolean b){
+        isOnBus = b;
+    }
+
+    public int getTicksSpentWaiting(){
+        return ticksSpentWaiting;
+    }
+
+    public int getTicksSpentTraveling(){
+        return ticksSpentTraveling;
+    }
+
+    public int getDestinationStopId(){
+        return destinationStopId;
+    }
 }
