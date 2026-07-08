5b06253ef847851dc59eb5275b956470a5f78d8d
voidSevenSevenSix <cobbsaladluv@gmail.com>
2026-07-07 21:57:19 -0500

    tired

--- route/Route.java
+++ route/Route.java
@@ -1,9 +1,17 @@
 package route;
 
+import java.util.ArrayList;
+
+import route.entity.Bus;
+import route.entity.Stop;
+
 public class Route {
-    
+    private ArrayList<Stop> stops;
+    private ArrayList<Bus> buses = new ArrayList<>();
 
-
+    public Route(ArrayList<Stop> stops){
+        this.stops = stops;
+    }
 
     public int getRandomStopAfterId(int id){
         return 0;
