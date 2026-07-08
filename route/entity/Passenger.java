900d17b00c5f45222c1068e8a0acba4e46a399d5
voidSevenSevenSix <cobbsaladluv@gmail.com>
2026-07-07 20:02:11 -0500

    add stop class

--- route/entity/Passenger.java
+++ route/entity/Passenger.java
@@ -3,10 +3,9 @@ package route.entity;
 public class Passenger {
     private final int destinationStopId;
 
-    private int ticksSpentWaiting = 0;
-    private int ticksSpentTraveling = 0;
+    private int ticksSpentWaiting = 0; // ticks spent waiting at the stop
+    private int ticksSpentTraveling = 0; // ticks spent both waiting at the stop and on the bus
     private boolean isOnBus = false;
-
 
     public Passenger(int destinationStopId){
         this.destinationStopId = destinationStopId;
@@ -19,6 +18,10 @@ public class Passenger {
         ticksSpentTraveling++;
     }
 
+    /**
+     * 
+     * @param b value to set the Passenger's isOnBus flag to
+     */
     public void setOnBus(boolean b){
         isOnBus = b;
     }
--- route/entity/Stop.java
+++ route/entity/Stop.java
@@ -1,5 +1,53 @@
 package route.entity;
 
+import java.util.ArrayList;
+
 public class Stop {
-    
+    private final int id;
+
+    private ArrayList<Passenger> passengers = new ArrayList<>();
+    private double passengersGeneratedPerTick;
+    private double passengerGenerationCounter = 0;
+
+    public Stop(int id, double passengersGeneratedPerTick){
+        this.id = id;
+        this.passengersGeneratedPerTick = passengersGeneratedPerTick;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    /**
+     * 
+     * @param p Passenger to add to this Stop's waiting Passengers
+     */
+    public void addPassenger(Passenger p){
+        passengers.add(p);
+    }
+
+    /**
+     *  
+     * @return A List of Passengers to board onto the Bus at this Stop; calling this method also clears this Stop's list of Passengers
+     */
+    public ArrayList<Passenger> getPassengersForBoarding(){
+        ArrayList<Passenger> toBoard = new ArrayList<>(passengers);
+        passengers.clear();
+        return toBoard;
+    }
+
+    /**
+     * 
+     * @param destinationStopId stop ID from (this stop, terminal]
+     */
+    public void tick(int destinationStopId){
+        passengerGenerationCounter+=passengersGeneratedPerTick;
+        if(passengerGenerationCounter>=1){
+            passengerGenerationCounter--;
+            addPassenger(new Passenger(destinationStopId));
+        }
+        for(Passenger p : passengers){
+            p.tick();
+        }
+    }
 }
